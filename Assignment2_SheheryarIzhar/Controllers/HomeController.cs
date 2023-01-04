c1604e736dfb6904d6c565dbf1785374bfccf8a8
sheheryar72 <86529804+sheheryar72@users.noreply.github.com>
2023-01-04 05:28:43 +0500

    first commit

---
+++ Assignment2_SheheryarIzhar/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Assignment2_SheheryarIzhar.DataManager;
+using Assignment2_SheheryarIzhar.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment2_SheheryarIzhar.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult StudentList()
+        {
+            StudentManager studentManager = new StudentManager();
+            var students = studentManager.GetAllStudent();
+            return View(students);
+        }
+        public IActionResult AddStudent(Student student)
+        {
+            StudentManager studentManager = new StudentManager();
+            studentManager.AddStudent(student);
+            return RedirectToAction("StudentList", "Home");
+        }
+        public IActionResult EditStudent(Student student)
+        {
+            StudentManager studentManager = new StudentManager();
+            studentManager.EditStudent(student);
+            return RedirectToAction("StudentList", "Home");
+        }
+    }
+}
---
+++ Assignment2_SheheryarIzhar/DataManager/StudentManager.cs
@@ -0,0 +1,100 @@
+﻿using Assignment2_SheheryarIzhar.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Assignment2_SheheryarIzhar.DataManager
+{
+    public class StudentManager
+    {
+        string _connectionString = "Data Source=.;Initial Catalog=StudentRecords;Integrated Security=true";
+        public List<Student> GetAllStudent()
+        {
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                SqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "Select * from Student";
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                DataTable dt = new DataTable();
+                dt.Load(reader);
+                if(dt != null && dt.Rows.Count > 0)
+                {
+                    List<Student> st = new List<Student>();
+                    for (int i = 0; i < dt.Rows.Count; i++)
+                    {
+                        var student = new Student();
+                        student.Id = Convert.ToInt32(dt.Rows[i]["Id"]);
+                        student.Name = dt.Rows[i]["Name"].ToString();
+                        student.Age = Convert.ToInt32(dt.Rows[i]["Age"]);
+                        st.Add(student);
+                    }
+                    conn.Close();
+                    return st;
+                }
+                return null;
+            }
+        }
+        public Student GetStudentById(int Id)
+        {
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                SqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "Select * from Student Where Id = @id";
+                cmd.Parameters.AddWithValue("@Id", Id);
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                DataTable dt = new DataTable();
+                dt.Load(reader);
+                if (dt != null && dt.Rows.Count > 0)
+                {
+                    var student = new Student();
+                    student.Name = dt.Rows[0]["Name"].ToString();
+                    student.Age = Convert.ToInt32(dt.Rows[0]["Age"]);
+                    return student;
+                }
+                return null;
+            }
+        }
+        public string AddStudent(Student student)
+        {
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                SqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "Insert into Student (Name, Age) values (@Name, @Age)";
+                cmd.Parameters.AddWithValue("@Name", student.Name);
+                cmd.Parameters.AddWithValue("@Age", student.Age);
+                conn.Open();
+                int rawAffected = cmd.ExecuteNonQuery();
+
+                if(rawAffected > 0)
+                {
+                    return student.Name;
+                    conn.Close();
+                }
+                return null;
+            }
+        }
+        public string EditStudent(Student student)
+        {
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                SqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "Update Student Set Name = @Name, Age = @Age where Id = @Id";
+                cmd.Parameters.AddWithValue("@Id", student.Id);
+                cmd.Parameters.AddWithValue("@Name", student.Name);
+                cmd.Parameters.AddWithValue("@Age", student.Age);
+                conn.Open();
+                int rawAffected = cmd.ExecuteNonQuery();
+
+                if (rawAffected > 0)
+                {
+                    return student.Name;
+                    conn.Close();
+                }
+                return null;
+            }
+        }
+    }
+}
